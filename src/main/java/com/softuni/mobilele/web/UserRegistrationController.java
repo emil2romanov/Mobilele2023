092b02bf759d8e3ad91e7d176c3ab7fccdfacd2c
Emil Romanov <eromanov8@gmail.com>
2023-09-28 16:39:15 +0300

    Hash the user password

--- src/main/java/com/softuni/mobilele/web/UserRegistrationController.java
+++ src/main/java/com/softuni/mobilele/web/UserRegistrationController.java
@@ -27,6 +27,8 @@ public class UserRegistrationController {
     @PostMapping("/register")
     public String register(UserRegistrationDTO userRegistrationDTO) {
 
+        // TODO: Registration email with activation link
+
         userService.registerUser(userRegistrationDTO);
 
         return "redirect:/";
