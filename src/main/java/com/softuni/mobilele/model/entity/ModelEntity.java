cf8281fe7a7b94f0468528bcf2a18b7c92dafa3c
Emil Romanov <eromanov8@gmail.com>
2023-10-03 15:54:19 +0300

    model and offer entity data

--- src/main/java/com/softuni/mobilele/model/entity/ModelEntity.java
+++ src/main/java/com/softuni/mobilele/model/entity/ModelEntity.java
@@ -1,9 +1,44 @@
 package com.softuni.mobilele.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.softuni.mobilele.model.enums.ModelCategoryEnum;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "models")
-public class ModelEntity extends BaseEntity{
+public class ModelEntity extends BaseEntity {
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private ModelCategoryEnum category;
+
+    @ManyToOne
+    private BrandEntity brand;
+
+    public String getName() {
+        return name;
+    }
+
+    public ModelEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ModelCategoryEnum getCategory() {
+        return category;
+    }
+
+    public ModelEntity setCategory(ModelCategoryEnum category) {
+        this.category = category;
+        return this;
+    }
+
+    public BrandEntity getBrand() {
+        return brand;
+    }
+
+    public ModelEntity setBrand(BrandEntity brand) {
+        this.brand = brand;
+        return this;
+    }
 }
---
+++ src/main/java/com/softuni/mobilele/model/enums/EngineEnum.java
@@ -0,0 +1,5 @@
+package com.softuni.mobilele.model.enums;
+
+public enum EngineEnum {
+    PETROL, DIESEL, ELECTRIC;
+}
