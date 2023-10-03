6d786cc1b081402b77f249b71b93fd70c55d97c3
Emil Romanov <eromanov8@gmail.com>
2023-10-03 14:24:11 +0300

    Logging current user

--- src/main/java/com/softuni/mobilele/service/UserService.java
+++ src/main/java/com/softuni/mobilele/service/UserService.java
@@ -1,7 +1,10 @@
 package com.softuni.mobilele.service;
 
+import com.softuni.mobilele.model.dto.UserLoginDTO;
 import com.softuni.mobilele.model.dto.UserRegistrationDTO;
 
 public interface UserService {
     void registerUser(UserRegistrationDTO userRegistrationDTO);
+
+    boolean loginUser(UserLoginDTO userLoginDTO);
 }
