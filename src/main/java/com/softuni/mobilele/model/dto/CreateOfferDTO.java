2594bc9cab12c4910c1ab8594280327db8395ffe
Emil Romanov <eromanov8@gmail.com>
2023-10-04 11:26:12 +0300

    creating record for offer creation

--- src/main/java/com/softuni/mobilele/model/dto/CreateOfferDTO.java
+++ src/main/java/com/softuni/mobilele/model/dto/CreateOfferDTO.java
@@ -1,4 +1,14 @@
 package com.softuni.mobilele.model.dto;
 
-public record CreateOfferDTO () {
+import com.softuni.mobilele.model.enums.EngineEnum;
+import com.softuni.mobilele.model.enums.TransmissionEnum;
+
+public record CreateOfferDTO (String description,
+                              Long modelId,
+                              EngineEnum engine,
+                              TransmissionEnum transmission,
+                              String imageUrl,
+                              Integer mileage,
+                              Integer price,
+                              Integer year) {
 }
