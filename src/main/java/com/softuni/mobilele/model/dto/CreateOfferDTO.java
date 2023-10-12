9da4d28bce535ba0c232ac225e46ce2e8cd2635f
Emil Romanov <eromanov8@gmail.com>
2023-10-12 13:46:40 +0300

    more offer features

--- src/main/java/com/softuni/mobilele/model/dto/CreateOfferDTO.java
+++ src/main/java/com/softuni/mobilele/model/dto/CreateOfferDTO.java
@@ -4,29 +4,18 @@ import com.softuni.mobilele.model.enums.EngineEnum;
 import com.softuni.mobilele.model.enums.TransmissionEnum;
 import jakarta.validation.constraints.*;
 
-public record CreateOfferDTO (
-        @NotEmpty
-        @Size(min = 5, max = 512)
-        String description,
-        @Positive
-        @NotNull
-        Long modelId,
-        @NotNull
-        EngineEnum engine,
-        @NotNull
-        TransmissionEnum transmission,
-        @NotEmpty
-        String imageUrl,
-        @Positive
-        @NotNull
-        Integer mileage,
-        @Positive
-        @NotNull
-        Integer price,
-        @NotNull
+public record CreateOfferDTO(
+        @NotEmpty @Size(min = 5, max = 512) String description,
+        @Positive @NotNull Long modelId, @NotNull EngineEnum engine,
+        @NotNull TransmissionEnum transmission, @NotEmpty String imageUrl,
+        @Positive @NotNull Integer mileage,
+        @Positive @NotNull Integer price,
+        //@YearNotInTheFuture(message = "The year should not be in the future!")
+        @NotNull(message = "Year must be provided!")
         @Min(1930)
         Integer year) {
-    public CreateOfferDTO () {
-        this(null, null, null, null, null, null, null, null);
+
+    public static CreateOfferDTO empty() {
+        return new CreateOfferDTO(null, null, null, null, null, null, null, null);
     }
 }
--- src/main/java/com/softuni/mobilele/web/OfferController.java
+++ src/main/java/com/softuni/mobilele/web/OfferController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.UUID;
 
@@ -39,7 +40,7 @@ public class OfferController {
     @GetMapping("/add")
     public String add(Model model) {
 
-        if (model.containsAttribute("createOfferDTO")) {
+        if (!model.containsAttribute("createOfferDTO")) {
             model.addAttribute("createOfferDTO", new CreateOfferDTO());
         }
 
@@ -50,7 +51,15 @@ public class OfferController {
 
     @PostMapping("/add")
     public String add(@Valid CreateOfferDTO createOfferDTO,
-                      BindingResult bindingResult) {
+                      BindingResult bindingResult,
+                      RedirectAttributes rAtt) {
+
+        if (bindingResult.hasErrors()) {
+            rAtt.addFlashAttribute("createOfferDTO", createOfferDTO);
+            rAtt.addFlashAttribute("org.springframework.validation.BindingResult.createOfferDTO", bindingResult);
+
+            return "redirect:/offer/add";
+        }
 
         offerService.createOffer(createOfferDTO);
 
