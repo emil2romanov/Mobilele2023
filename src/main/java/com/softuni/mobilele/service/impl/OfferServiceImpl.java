cad2386ed6acb6b4ee7611b677af6e432763690e
Emil Romanov <eromanov8@gmail.com>
2023-10-12 14:58:57 +0300

    doing the createOffer in OfferServiceImpl

--- src/main/java/com/softuni/mobilele/service/impl/OfferServiceImpl.java
+++ src/main/java/com/softuni/mobilele/service/impl/OfferServiceImpl.java
@@ -1,25 +1,50 @@
 package com.softuni.mobilele.service.impl;
 
 import com.softuni.mobilele.model.dto.CreateOfferDTO;
+import com.softuni.mobilele.model.entity.ModelEntity;
+import com.softuni.mobilele.model.entity.OfferEntity;
+import com.softuni.mobilele.repository.ModelRepository;
 import com.softuni.mobilele.repository.OfferRepository;
 import com.softuni.mobilele.service.OfferService;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
 public class OfferServiceImpl implements OfferService {
 
     private final OfferRepository offerRepository;
+    private final ModelRepository modelRepository;
 
-    public OfferServiceImpl(OfferRepository offerRepository) {
+    public OfferServiceImpl(OfferRepository offerRepository, ModelRepository modelRepository) {
         this.offerRepository = offerRepository;
+        this.modelRepository = modelRepository;
     }
 
     @Override
     public UUID createOffer(CreateOfferDTO createOfferDTO) {
-        // TODO - create offer
-        // throw new UnsupportedOperationException("Coming soon!");
-        return UUID.randomUUID();
+
+        OfferEntity newOffer = map(createOfferDTO);
+        ModelEntity modelEntity = modelRepository.findById(createOfferDTO.modelId()).orElseThrow(() ->
+                new IllegalArgumentException("Model with id " + createOfferDTO.modelId() + " not found!"));
+
+        newOffer.setModel(modelEntity);
+
+        newOffer = offerRepository.save(newOffer);
+
+        return newOffer.getUuid();
+    }
+
+    private OfferEntity map(CreateOfferDTO createOfferDTO) {
+        return new OfferEntity()
+                .setUuid(UUID.randomUUID())
+                .setDescription(createOfferDTO.description())
+                .setEngine(createOfferDTO.engine())
+                .setTransmission(createOfferDTO.transmission())
+                .setImageUrl(createOfferDTO.imageUrl())
+                .setMileage(createOfferDTO.mileage())
+                .setPrice(BigDecimal.valueOf(createOfferDTO.price()))
+                .setYear(createOfferDTO.year());
     }
 }
