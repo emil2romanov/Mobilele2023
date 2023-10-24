3bb5bd2b2191e5249a3cdd894ac6418b601f2309
Emil Romanov <eromanov8@gmail.com>
2023-10-24 16:36:27 +0300

    more exchange rate features

--- src/main/java/com/softuni/mobilele/config/AppConfig.java
+++ src/main/java/com/softuni/mobilele/config/AppConfig.java
@@ -1,9 +1,14 @@
 package com.softuni.mobilele.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
 
 @Configuration
 public class AppConfig {
@@ -11,5 +16,12 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 }
--- src/main/java/com/softuni/mobilele/init/RatesInit.java
+++ src/main/java/com/softuni/mobilele/init/RatesInit.java
@@ -1,13 +1,20 @@
 package com.softuni.mobilele.init;
 
+import com.softuni.mobilele.config.OpenExchangeRateConfig;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RatesInit implements CommandLineRunner {
 
+    private final OpenExchangeRateConfig openExchangeRateConfig;
+
+    public RatesInit(OpenExchangeRateConfig openExchangeRateConfig) {
+        this.openExchangeRateConfig = openExchangeRateConfig;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.println(openExchangeRateConfig);
     }
 }
