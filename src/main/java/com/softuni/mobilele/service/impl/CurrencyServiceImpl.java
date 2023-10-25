5e654334181191cdd87507803b7ee7a0a92880d9
Emil Romanov <eromanov8@gmail.com>
2023-10-25 10:51:02 +0300

    initial commit

--- src/main/java/com/softuni/mobilele/service/impl/CurrencyServiceImpl.java
+++ src/main/java/com/softuni/mobilele/service/impl/CurrencyServiceImpl.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 public class CurrencyServiceImpl implements CurrencyService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyServiceImpl.class);
+
     private final ExchangeRateRepository exchangeRateRepository;
 
     public CurrencyServiceImpl(ExchangeRateRepository exchangeRateRepository) {
@@ -32,53 +33,62 @@ public class CurrencyServiceImpl implements CurrencyService {
         BigDecimal BGN_TO_EUR = getExchangeRate(exchangeRatesDTO, "BGN", "EUR").orElse(null);
 
         if (BGN_TO_USD != null) {
-            ExchangeRateEntity exchangeRateEntity = new ExchangeRateEntity().setCurrency("USD").setRate(BGN_TO_USD);
+            ExchangeRateEntity exchangeRateEntity =
+                    new ExchangeRateEntity().setCurrency("USD").setRate(BGN_TO_USD);
             exchangeRateRepository.save(exchangeRateEntity);
         } else {
-            LOGGER.error("Unable to get exchange rate for BGN to USD");
+            LOGGER.error("Unable to get exchange rate for BGN TO USD");
         }
 
         if (BGN_TO_EUR != null) {
-            ExchangeRateEntity exchangeRateEntity = new ExchangeRateEntity().setCurrency("EUR").setRate(BGN_TO_EUR);
+            ExchangeRateEntity exchangeRateEntity =
+                    new ExchangeRateEntity().setCurrency("EUR").setRate(BGN_TO_EUR);
             exchangeRateRepository.save(exchangeRateEntity);
         } else {
-            LOGGER.error("Unable to get exchange rate for BGN to EUR");
+            LOGGER.error("Unable to get exchange rate for BGN TO EUR");
         }
 
         LOGGER.info("Rates refreshed...");
     }
 
-    private static Optional<BigDecimal> getExchangeRate(ExchangeRatesDTO exchangeRatesDTO,
-                                                        String from, String to) {
+    private static Optional<BigDecimal> getExchangeRate(
+            ExchangeRatesDTO exchangeRatesDTO,
+            String from,
+            String to
+    ) {
 
         Objects.requireNonNull(from, "From currency cannot be null");
         Objects.requireNonNull(to, "To currency cannot be null");
+
 
         // e.g. USD -> USD
         if (Objects.equals(from, to)) {
             return Optional.of(BigDecimal.ONE);
         }
 
-
         if (from.equals(exchangeRatesDTO.base())) {
             // e.g. USD -> BGN
             if (exchangeRatesDTO.rates().containsKey(to)) {
                 return Optional.of(exchangeRatesDTO.rates().get(to));
             }
-        } else if (Objects.equals(to, exchangeRatesDTO.base())) {
+        } else if (Objects.equals(to, exchangeRatesDTO.base())){
             // e.g. BGN -> USD
             if (exchangeRatesDTO.rates().containsKey(from)) {
                 return Optional.of(BigDecimal.ONE.divide(
-                        exchangeRatesDTO.rates().get(from), 3, RoundingMode.DOWN
+                        exchangeRatesDTO.rates().get(from),
+                        3,
+                        RoundingMode.DOWN
                 ));
             }
         } else if (exchangeRatesDTO.rates().containsKey(from) &&
-                   exchangeRatesDTO.rates().containsKey(to)) {
+                exchangeRatesDTO.rates().containsKey(to)) {
             return Optional.of(
                     exchangeRatesDTO.rates().get(to)
-                    .divide(exchangeRatesDTO.rates().get(from),
-                            3, RoundingMode.DOWN));
+                            .divide(exchangeRatesDTO.rates().get(from),
+                                    3, RoundingMode.DOWN)
+            );
         }
+
         return Optional.empty();
     }
 }
