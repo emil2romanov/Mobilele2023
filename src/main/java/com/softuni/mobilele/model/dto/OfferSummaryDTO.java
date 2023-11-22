7dd9df1efd82adbdd17b485ae36377bb7d896762
Emil Romanov <eromanov8@gmail.com>
2023-11-22 23:46:42 +0200

    creating dto for offer summary

---
+++ src/main/java/com/softuni/mobilele/model/dto/OfferSummaryDTO.java
@@ -0,0 +1,17 @@
+package com.softuni.mobilele.model.dto;
+
+import com.softuni.mobilele.model.enums.EngineEnum;
+import com.softuni.mobilele.model.enums.TransmissionEnum;
+
+import java.math.BigDecimal;
+
+public record OfferSummaryDTO (
+        String brand,
+        String model,
+        int year,
+        int mileage,
+        BigDecimal price,
+        EngineEnum engine,
+        TransmissionEnum transmission
+) {
+}
--- src/main/java/com/softuni/mobilele/service/OfferService.java
+++ src/main/java/com/softuni/mobilele/service/OfferService.java
@@ -1,10 +1,14 @@
 package com.softuni.mobilele.service;
 
 import com.softuni.mobilele.model.dto.CreateOfferDTO;
+import com.softuni.mobilele.model.dto.OfferSummaryDTO;
+import org.springframework.data.domain.Page;
 
 import java.util.UUID;
 
 public interface OfferService {
 
     UUID createOffer(CreateOfferDTO createOfferDTO);
+
+    Page<OfferSummaryDTO>
 }
