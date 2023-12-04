4aba02faf76fd01a38a8ba10ceab8a07c3cbf1a8
Emil Romanov <eromanov8@gmail.com>
2023-12-04 16:41:41 +0200

    initial commit

--- src/main/java/com/softuni/mobilele/service/impl/UserActivationServiceImpl.java
+++ src/main/java/com/softuni/mobilele/service/impl/UserActivationServiceImpl.java
@@ -6,9 +6,14 @@ import com.softuni.mobilele.service.UserActivationService;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 @Service
 public class UserActivationServiceImpl implements UserActivationService {
 
+    private static final String ACTIVATION_CODE_SYMBOLS = "abcdefghijklmnopqrstuvwxyzABCDEFGJKLMNPRSTUVWXYZ0123456789";
+    private static final int ACTIVATION_CODE_LENGTH = 20;
     private final EmailService emailService;
 
     public UserActivationServiceImpl(EmailService emailService) {
@@ -26,5 +31,23 @@ public class UserActivationServiceImpl implements UserActivationService {
     public void cleanUpObsoleteActivationLinks() {
         //TODO: Implement
         System.out.println("NOT YET");
+    }
+
+    @Override
+    public String createActivationCode(String userEmail) {
+
+    }
+
+    private static String generateActivationCode() {
+
+        StringBuilder activationCode = new StringBuilder();
+        Random random = new SecureRandom();
+
+        for (int i = 0; i < ACTIVATION_CODE_LENGTH; i++) {
+            int randInx = random.nextInt(ACTIVATION_CODE_SYMBOLS.length());
+            activationCode.append(ACTIVATION_CODE_SYMBOLS.charAt(randInx));
+        }
+
+        return activationCode.toString();
     }
 }
