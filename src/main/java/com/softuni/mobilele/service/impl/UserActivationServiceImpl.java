7adb3ec1694968047332f2e7c67091550c0eb61e
Emil Romanov <eromanov8@gmail.com>
2023-12-05 11:33:20 +0200

    activation code

--- src/main/java/com/softuni/mobilele/service/impl/UserActivationServiceImpl.java
+++ src/main/java/com/softuni/mobilele/service/impl/UserActivationServiceImpl.java
@@ -1,12 +1,17 @@
 package com.softuni.mobilele.service.impl;
 
+import com.softuni.mobilele.model.entity.UserActivationCodeEntity;
 import com.softuni.mobilele.model.events.UserRegisteredEvent;
+import com.softuni.mobilele.repository.UserActivationCodeRepository;
+import com.softuni.mobilele.repository.UserRepository;
 import com.softuni.mobilele.service.EmailService;
 import com.softuni.mobilele.service.UserActivationService;
+import com.softuni.mobilele.service.exception.ObjectNotFoundException;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
+import java.time.Instant;
 import java.util.Random;
 
 @Service
@@ -15,15 +20,20 @@ public class UserActivationServiceImpl implements UserActivationService {
     private static final String ACTIVATION_CODE_SYMBOLS = "abcdefghijklmnopqrstuvwxyzABCDEFGJKLMNPRSTUVWXYZ0123456789";
     private static final int ACTIVATION_CODE_LENGTH = 20;
     private final EmailService emailService;
+    private final UserRepository userRepository;
+    private final UserActivationCodeRepository userActivationCodeRepository;
 
-    public UserActivationServiceImpl(EmailService emailService) {
+    public UserActivationServiceImpl(EmailService emailService, UserRepository userRepository, UserActivationCodeRepository userActivationCodeRepository) {
         this.emailService = emailService;
+        this.userRepository = userRepository;
+        this.userActivationCodeRepository = userActivationCodeRepository;
     }
 
     @Override
     @EventListener(UserRegisteredEvent.class)
     public void userRegistered(UserRegisteredEvent event) {
-        // TODO: ADD Activation links
+        String activationCode = createActivationCode(event.getUserEmail());
+
         emailService.sendRegistrationEmail(event.getUserEmail(), event.getUserNames());
     }
 
@@ -36,6 +46,16 @@ public class UserActivationServiceImpl implements UserActivationService {
     @Override
     public String createActivationCode(String userEmail) {
 
+        String activationCode = generateActivationCode();
+
+        UserActivationCodeEntity userActivationCodeEntity = new UserActivationCodeEntity();
+        userActivationCodeEntity.setActivationCode(activationCode);
+        userActivationCodeEntity.setCreated(Instant.now());
+        userActivationCodeEntity.setUser(userRepository.findByEmail(userEmail).orElseThrow(() -> new ObjectNotFoundException("User not found")));
+
+        userActivationCodeRepository.save(userActivationCodeEntity);
+
+        return activationCode;
     }
 
     private static String generateActivationCode() {
